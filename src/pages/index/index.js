ddc7a678a77acd4bd9ee89e673c0d421b8b1aa7f
duwenzheng <15630120659@163.com>
2019-05-20 14:36:01 +0800

    test

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -23,7 +23,7 @@ import './index.scss'
 class Index extends Component {
 
     config = {
-    navigationBarTitleText: '首页'
+    navigationBarTitleText: '牧星校园'
   }
 
   componentWillReceiveProps (nextProps) {
